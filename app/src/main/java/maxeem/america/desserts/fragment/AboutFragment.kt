f4ddd50749c6211345399bf6c2ca09813c8dc15d
Maxeem <jmax.posh@gmail.com>
2019-08-28 08:29:17 +0300

    improve databinding in about layout

--- app/src/main/java/maxeem/america/desserts/fragment/AboutFragment.kt
+++ app/src/main/java/maxeem/america/desserts/fragment/AboutFragment.kt
@@ -1,9 +1,7 @@
 package maxeem.america.desserts.fragment
 
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -13,11 +11,9 @@ import org.jetbrains.anko.AnkoLogger
 
 class AboutFragment : Fragment(), AnkoLogger {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val binding = FragmentAboutBinding.inflate(inflater, container, false)
-        binding.model = viewModels<AboutModel>().value
-        binding.description.movementMethod = LinkMovementMethod.getInstance()
-        return binding.root
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        FragmentAboutBinding.inflate(inflater, container, false).apply {
+            model = viewModels<AboutModel>().value
+        }.root
 
 }
